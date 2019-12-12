64de5e3e7fa13f4b34eda52d8dc17a49f7f1d510
fkchaud <faku_imper@hotmail.com.ar>
2019-12-12 16:55:45 -0300

    Add addition and edition in custom FAQs (#9)

--- co-organizer/src/script.js
+++ co-organizer/src/script.js
@@ -1,15 +1,69 @@
 function cleanModal() {
     document.getElementById("question-textarea").value = "";
     document.getElementById("answer-textarea").value = "";
+    document.getElementById("question-id").value = "0";
     document.getElementById("faqModalLabel").innerHTML = "Add a question";
 }
 
 function editModal(event) {
-    document.getElementById("question-textarea").value = event.parentNode.parentNode.parentNode.children[0].innerHTML;
-    document.getElementById("answer-textarea").value = event.parentNode.parentNode.parentNode.children[1].innerHTML;
+    document.getElementById("question-textarea").value = event.parentNode.parentNode.parentNode.children[1].innerHTML;
+    document.getElementById("answer-textarea").value = event.parentNode.parentNode.parentNode.children[2].innerHTML;
+    document.getElementById("question-id").value = event.parentNode.parentNode.parentNode.children[0].innerHTML;
     document.getElementById("faqModalLabel").innerHTML = "Edit a question";
 }
 
+function searchFAQ(id) {
+    let idClasses = document.getElementsByClassName("question-identifier");
+    let found;
+
+    for (var i = 0; i < idClasses.length; i++) {
+        if (idClasses[i].innerHTML == id) {
+            found = idClasses[i].parentNode;
+            break;
+        }
+    }
+    return found;
+}
+
+function saveModal() {
+    let question = document.getElementById("question-textarea").value;
+    let answer = document.getElementById("answer-textarea").value;
+    let id = document.getElementById("question-id").value;
+
+    let questionRow = searchFAQ(id);
+
+    if (questionRow == null) {
+        let faqTable = document.getElementById("faq-table");
+        questionRow = document.createElement("tr");
+
+        let idClasses = document.getElementsByClassName("question-identifier");
+        id = parseInt(idClasses[idClasses.length - 1].innerHTML) + 1;
+
+        let idTd = document.createElement("td");
+        idTd.innerHTML = id;
+        idTd.classList.add("question-identifier");
+        questionRow.appendChild(idTd);
+
+        let questionTd = document.createElement("td");
+        questionRow.appendChild(questionTd);
+
+        let answerTd = document.createElement("td");
+        questionRow.appendChild(answerTd);
+
+        let buttonsTd = document.createElement("td");
+        buttonsTd.innerHTML = '<div class="btn-group">' + 
+                                  '<button type="button" class="btn btn-default" data-toggle="modal" data-target="#faqModal" onclick="editModal(this)">Edit</button>' +
+                                  '<button class="btn btn-default btn-danger" onclick="deleteRow(this)">Delete</button>' +
+                              '</div>';
+        buttonsTd.classList.add("fit");
+        questionRow.appendChild(buttonsTd);
+
+        faqTable.appendChild(questionRow);
+    }
+
+    questionRow.children[1].innerHTML = question;
+    questionRow.children[2].innerHTML = answer;
+}
 function togglePublic(event) {
     let isPublic;
     isPublic = event.parentNode.parentNode.parentNode.children[3].innerHTML;
